3751f7d5004802045afca11cf275dbb83887c2ac
muhF1508 <firdausbarra08@gmail.com>
2018-10-20 13:16:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mbarrafirdaus/notes/Splash.java
@@ -0,0 +1,27 @@
+package com.example.mbarrafirdaus.notes;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mbarrafirdaus.notes.view.MainActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000L);
+    }
+}
